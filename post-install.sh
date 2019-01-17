067c330c5f9599c08d655ea8b8a0dfc34261addd
HazCod <HazCod@users.noreply.github.com>
2019-01-17 22:36:35 +0100

    Remove secscan after use

--- post-install.sh
+++ post-install.sh
@@ -4,8 +4,8 @@
 set -e
 set -o pipefail
 
-# Do a security scan
-$APP_DIR/secscan.sh
+# Do a security scan and remove it
+$APP_DIR/secscan.sh && rm "$APP_DIR/secscan.sh"
 
 # remove apk package manager
 find / -type f -iname '*apk*' -xdev -delete
